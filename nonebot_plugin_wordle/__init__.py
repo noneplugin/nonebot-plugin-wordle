30fa177786df4213c3049ed6300901fa8faeed43
meetwq <meetwq@gmail.com>
2023-04-13 12:03:01 +0800

    0.2.1

--- nonebot_plugin_wordle/__init__.py
+++ nonebot_plugin_wordle/__init__.py
@@ -53,7 +53,7 @@ __plugin_meta__ = PluginMetadata(
         "unique_name": "wordle",
         "example": "@小Q 猜单词\nwordle -l 6 -d CET6",
         "author": "meetwq <meetwq@gmail.com>",
-        "version": "0.2.0",
+        "version": "0.2.1",
     },
 )
 
